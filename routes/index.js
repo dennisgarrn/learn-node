285757cdeaca0afdd84042996139fc38a06e2017
Dennis Garrn <dennis.garrn@pwc-digital.ch>
2018-11-20 16:16:53 +0100

    remove whitespace in 'add' route

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ const { catchErrors } = require('./../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/stores/page/:page', catchErrors(storeController.getStores));
-router.get('/add ', authController.isLoggedIn, storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post('/add', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.createStore));
 router.post('/add/:id', storeController.upload, catchErrors(storeController.resize), catchErrors(storeController.updateStore));
